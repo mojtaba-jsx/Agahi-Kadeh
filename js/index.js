68bfe3aecd2df7c86a87bdf545d4c8cdd8bccdcc
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-06-16 11:39:22 +0330

    [HTML-CSS-JS]- Add Css Styles For Header Section And Add Particle Js To Main Page

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// * Initial Particle Js
+particlesJS.load(
+  "particles-js",
+  "./vendor/particle-js/particle-config.json",
+  function () {
+    console.log("callback - particles.js config loaded");
+  }
+);
